27fe159d760ecc0444941ad4d77faedc4b72ffbd
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 13:30:53 +0100

    commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 7 : Partie 3</title>
+  </head>
+  <body>
+    <?php
+    for ($number = 1; $number <= 100; $number++){
+    echo 'On y arrive presque.' . ' ';
+    }
+    ?>
+  </body>
+</html>
